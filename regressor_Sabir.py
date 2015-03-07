96ccfcddbcd31fbd8d01fac176479ae2a54717be
akhadov.sabir@hotmail.com <akhadov.sabir@hotmail.com>
2015-03-07 10:14:03 +0100

    Ridgenot good

--- regressor_Sabir.py
+++ regressor_Sabir.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as mplt
 import csv
 import sklearn.cross_validation as skcv
+import sklearn.linear_model as sklin
+import sklearn.grid_search as skgs
 
 import datetime
 
@@ -13,12 +15,11 @@ def read_data (inpath):
 		for row in reader:
 			t = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
 			X.append(get_features(t.hour))
-			Xyear.append(get_features(t.year))
 		return np.atleast_2d(X)
 
 def get_features(h): #can be h^2...e
 	r = []
-	for p in range (1, degree+1):
+	for p in range (0, degree+1):
 		r.append(np.power(h, p)) #polynom
 	return r
 
@@ -34,20 +35,18 @@ print ('degree', degree)
 X = read_data('data/train.csv')
 Y = np.genfromtxt('data/train_y.csv', delimiter=',')
 
-print X.shape
-print Y.shape
 Xtrain, Xtest, Ytrain, Ytest = skcv.train_test_split(X,Y, train_size=0.75)
 
 
-import sklearn.linear_model as sklin
 
 regressor = sklin.LinearRegression()
+
 regressor.fit(Xtrain, Ytrain)
 
 Hplot = range (25)
 Xplot = np.atleast_2d([get_features(x) for x in Hplot])
 Yplot = regressor.predict(Xplot)
-mplt.plot(Xtrain[:,0], Ytrain, ('bo'))
+mplt.plot(Xtrain[:,1], Ytrain, ('bo'))
 mplt.plot(Hplot, Yplot,'r', linewidth=3)
 mplt.xlim([-0.5, 23.5])
 mplt.ylim([-1, 1000])
@@ -63,5 +62,17 @@ scorefun = skmet.make_scorer(logscore)
 scores = skcv.cross_val_score(regressor, X, Y, scoring=scorefun, cv=5)
 print ('C-V score = ', np.mean(scores), '+/-', np.std(scores))
 
+regressor_ridge = sklin.Ridge()
+param_grid = {'alpha': np.linspace(0, 1000, 1)}
+neg_scorefun = skmet.make_scorer(lambda x, y: -logscore(x, y))
+grid_search = skgs.GridSearchCV(regressor_ridge, param_grid, scoring=neg_scorefun, cv=100)
+grid_search.fit(Xtrain, Ytrain)
+
+best = grid_search.best_estimator_
+print(best)
+print('best score =', -grid_search.best_score_)
+
+
+#~ mplt.plot(Ypred-Ytest)
 
 mplt.show()
