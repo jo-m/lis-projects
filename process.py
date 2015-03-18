e29131674b6a37d948da658beb007e8e113d1e16
Jonathan Mueller <j.mueller@apoveda.ch>
2015-03-18 16:33:39 +0100

    continue with RandomForestClassifier

--- process.py
+++ process.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import svm
+from sklearn import ensemble
 
 from lib import *
 
 
 X = load_X('train')
 Y = load_Y('train')
-Xtrain, Xtest, Ytrain, Ytest = train_test_split_pd(X, Y, train_size=.1)
+Xtrain, Xtest, Ytrain, Ytest = train_test_split_pd(X, Y, train_size=.8)
 print 'Training data: ', Xtrain.shape, Ytrain.shape
 print 'Test data: ', Xtest.shape, Ytest.shape
 
@@ -29,8 +26,9 @@ def pred(X, clf1, clf2):
     y2 = clf2.predict(X)
     return pd.DataFrame(dict(y1=y1, y2=y2))
 
-clf1 = svm.SVC(kernel='linear')
-clf2 = svm.SVC(kernel='linear')
+print 'n_estimators=50'
+clf1 = ensemble.RandomForestClassifier(n_estimators=50)
+clf2 = ensemble.RandomForestClassifier(n_estimators=50)
 train(Xtrain, Ytrain, clf1, clf2)
 
 # calculate score with test set
@@ -38,6 +36,8 @@ Ypred = pred(Xtest, clf1, clf2)
 print 'Score:', score(Ytest, Ypred), 'Grade: %d%%' % grade(score(Ytest, Ypred))
 
 # load, predict, write validation set
+# train again with whole X, Y
+train(X, Y, clf1, clf2)
 Xvalidate = load_X('validate')
 Yvalidate = pred(Xvalidate, clf1, clf2)
-write_Y('validate', Yvalidate)
+write_Y('validate2', Yvalidate)
