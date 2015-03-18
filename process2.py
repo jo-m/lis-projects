15607022a24b6a34bcde079868bc3da0bc8e7fba
Jonathan Mueller <j.mueller@apoveda.ch>
2015-03-18 16:16:40 +0100

    use whole set for validation prediction

--- process2.py
+++ process2.py
@@ -15,7 +15,7 @@ from lib import *
 
 X = load_X('train')
 Y = load_Y('train')
-Xtrain, Xtest, Ytrain, Ytest = train_test_split_pd(X, Y, train_size=.9)
+Xtrain, Xtest, Ytrain, Ytest = train_test_split_pd(X, Y, train_size=.8)
 print 'Training data: ', Xtrain.shape, Ytrain.shape
 print 'Test data: ', Xtest.shape, Ytest.shape
 
@@ -41,6 +41,8 @@ Ypred = pred(Xtest, clf1, clf2)
 print 'Score:', score(Ytest, Ypred), 'Grade: %d%%' % grade(score(Ytest, Ypred))
 
 # load, predict, write validation set
+# train again with whole X, Y
+train(X, Y, clf1, clf2)
 Xvalidate = load_X('validate')
 Yvalidate = pred(Xvalidate, clf1, clf2)
 write_Y('validate2', Yvalidate)
