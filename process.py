73811d1ff0c4b497082f1b04266afdfe2b4fb241
Jonathan Mueller <j.mueller@apoveda.ch>
2015-03-25 13:42:04 +0100

    crossval with using y1

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-import sklearn.ensemble as skens
+import pandas as pd
 import sklearn.cross_validation as skcv
+import sklearn.ensemble as skens
 import sklearn.metrics as skmet
+import sklearn.preprocessing as skpre
 
 from lib import *
 
@@ -15,12 +17,26 @@ class UseY1Classifier(object):
     clf1 = skens.RandomForestClassifier(n_estimators=50)
     clf2 = skens.RandomForestClassifier(n_estimators=50)
 
-    def fit(self, *x, **xx):
-        self.clf1.fit(*x, **xx)
+    def _trans_y(self, y):
+        y = np.atleast_2d(y).T
+        enc = skpre.OneHotEncoder(sparse=False)
+        enc.fit(y)
+        return enc.transform(y)
+
+    def fit(self, X, Y):
+        if isinstance(Y, pd.DataFrame):
+            Y = Y.as_matrix()
+            X = X.as_matrix()
+        X_y1 = np.concatenate([X, self._trans_y(Y[:, 0])], axis=1)
+        self.clf1.fit(X, Y[:, 0])
+        self.clf2.fit(X_y1, Y[:, 1])
         return self
 
-    def predict(self, *x, **xx):
-        return self.clf1.predict(*x, **xx)
+    def predict(self, X):
+        y1 = self.clf1.predict(X)
+        X_y1 = np.concatenate([X, self._trans_y(y1)], axis=1)
+        y2 = self.clf2.predict(X_y1)
+        return np.vstack([y1, y2]).T
 
     def get_params(self, *x, **xx):
         return {}
@@ -44,8 +60,6 @@ def cross_validate(clf):
 
 
 def predict_validation_set(clf):
-    # load, predict, write validation set
-    # train again with whole X, Y
     clf.fit(X, Y)
     Xvalidate = load_X('validate')
     Yvalidate = clf.predict(Xvalidate)
