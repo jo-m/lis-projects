57ecf05e51c0ca0d254a446befe7ecabe79ebbf0
Jonathan Mueller <j.mueller@apoveda.ch>
2015-03-31 13:34:19 +0200

    loading data and splitting works

--- lib.py
+++ lib.py
@@ -3,30 +3,38 @@
 
 from __future__ import unicode_literals
 import numpy as np
+
 import h5py
+
+
+def to_ndarray(h5_arr):
+    arr = np.zeros(h5_arr.shape, dtype='float32')
+    h5_arr.read_direct(arr)
+    return arr
 
 
 def load_data(fname):
     f = h5py.File('data/%s.h5' % fname, 'r')
-    print f.name
-    print f.keys()
     if 'label' in f.keys():
-        return f['data'], f['label']
+        return to_ndarray(f['data']), to_ndarray(f['label'])
     else:
-        return f['data'], None
+        return to_ndarray(f['data']), None
 
 
 def write_Y(fname, Y):
-    if Y.shape[1] != 1:
-        raise 'Y has invalid shape!'
-    np.savetxt('results/%s_y_pred.txt' % fname, Y, fmt='%d', delimiter=',')
+    if Y.ndim != 1:
+        raise Exception('Y has invalid shape!')
+    if Y.dtype != 'int32':
+        raise Exception('Y has invalid dtype!')
+    np.savetxt('results/%s_y_pred.txt' % fname,
+               Y, fmt=str('%d'), delimiter=',')
 
 
 def score(Ytruth, Ypred):
-    if Ytruth.shape[1] != 1:
-        raise 'Ytruth has invalid shape!'
-    if Ypred.shape[1] != 1:
-        raise 'Ypred has invalid shape!'
+    if Ytruth.ndim != 1:
+        raise Exception('Ytruth has invalid shape!')
+    if Ypred.ndim != 1:
+        raise Exception('Ypred has invalid shape!')
 
     sum = (Ytruth != Ypred).astype(float).sum().sum()
     return sum / np.product(Ytruth.shape)
--- process.py
+++ process.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+import sklearn.cross_validation as skcv
+
 from lib import *
 
 X, Y = load_data('train')
 
-print X, Y
+
+print X.shape, Y.shape
+
+Xtrain, Xtest, Ytrain, Ytest = \
+    skcv.train_test_split(X, Y, train_size=0.8)
+
+print Xtrain.shape, Ytrain.shape
+print Xtest.shape, Ytest.shape
