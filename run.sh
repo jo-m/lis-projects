3a7166e2e104485e5612b52889b569df76b1a259
Jonathan Mueller <j.mueller@apoveda.ch>
2015-04-02 15:03:45 +0200

    add possibility to run REPL

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 run="$1"
 [ -z "$1" ] && run='process.py'
 
+if [ "$run" = 'repl' ]; then
+    run=''
+fi
+
 # enable GPU support
 if [ `uname` = Darwin ] && [ -d '/Developer/NVIDIA/CUDA-7.0' ]; then
     export THEANO_FLAGS='cuda.root=/Developer/NVIDIA/CUDA-7.0,device=gpu,floatX=float32,force_device=True'
