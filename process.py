51196c47eab6573feb3f51ee8fe0abfad5117ff5
Jonathan Mueller <j.mueller@apoveda.ch>
2015-04-26 21:23:09 +0200

    reaching 0.23

--- process.py
+++ process.py
@@ -19,7 +19,7 @@ X, Y = load_data('train')
 X = skpre.StandardScaler().fit_transform(X)
 
 Xtrain, Xtest, Ytrain, Ytest = \
-    skcv.train_test_split(X, Y, train_size=0.01)
+    skcv.train_test_split(X, Y, train_size=0.8)
 
 lb = skpre.LabelBinarizer()
 lb.fit(Ytrain)
@@ -29,16 +29,18 @@ num_features = Xtrain.shape[1]
 
 layers0 = [('input', InputLayer),
            ('dense0', DenseLayer),
-           ('dropout', DropoutLayer),
+           ('dropout0', DropoutLayer),
            ('dense1', DenseLayer),
            ('output', DenseLayer)]
 
 
 clf = NeuralNet(layers=layers0,
-                input_shape=(None, num_features),
+
                 dense0_num_units=200,
-                dropout_p=0.5,
+                dropout0_p=0.5,
                 dense1_num_units=200,
+
+                input_shape=(None, num_features),
                 output_num_units=num_classes,
                 output_nonlinearity=softmax,
 
@@ -48,14 +50,17 @@ clf = NeuralNet(layers=layers0,
 
                 eval_size=0.2,
                 verbose=1,
-                max_epochs=20,
+                max_epochs=200,
                 regression=False)
 
 
-clf.fit(Xtrain, Ytrain)
-Ypred = clf.predict(Xtest)
-sc = score(Ytest, Ypred)
-print 'Testset score = %.4f Grade = %d%%' % (sc, grade(sc))
+with Timer('testset'):
+    clf.fit(Xtrain, Ytrain)
+    Ypred = clf.predict(Xtest)
+    sc = score(Ytest, Ypred)
+    print 'Testset score = %.4f Grade = %d%%' % (sc, grade(sc))
+
+quit()
 
 clf.fit(X, Y)
 Xvalidate, _ = load_data('validate')
