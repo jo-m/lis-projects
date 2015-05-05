bcc21f539de033c44715fbad3114a637fc40c40a
andreas128 <steger.a@gmx.at>
2015-05-05 19:38:21 +0200

    Found a lib that does exaclty what we need, but did not get to work it yet.

--- process.py
+++ process.py
@@ -4,6 +4,7 @@
 import sklearn.preprocessing as skpre
 
 from lib import *
+from seminb import *
 
 
 def preprocess_features(X):
@@ -13,3 +14,11 @@ X, Y = load_X('train'), load_Y('train')
 preprocess_features(X)
 
 print X.shape, Y.shape
+
+
+
+clf = SemiNB()
+
+clf.train(X.as_matrix(),Y.as_matrix())
+
+print clf.predict_proba_all(X.as_matrix())
